c7b33b0003152e4e2e4defb8a68655b3554ec6ec
AshleyAbramowiczGibbs <ashleyabramo2@gmail.com>
2018-10-08 20:09:58 -0500

    first commit

--- dictionary.rb
+++ dictionary.rb
@@ -8,6 +8,8 @@ word = gets.chomp
 response = HTTP.get ("https://api.wordnik.com/v4/word.json/#{word}/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key=ac6099e63826b8650f05e22c4cc08baa2f21668e3f16176fd")
 data = response.parse
 
+puts "Here are a few definitions for #{word}"
+
 def all_definitions(data)
 
   index = 0
